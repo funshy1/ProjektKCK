3a3cbb3e8713c6a37478da6f98d4e67f1d7b4dcd
yetju000 <yetju000steam@gmail.com>
2016-10-16 16:42:32 +0200

    konsola+ Main character

---
+++ core/src/com/mygdx/screens/GamePlayScreen.java
@@ -0,0 +1,18 @@
+package com.mygdx.screens;
+
+import com.mygdx.game.ProjektKCK;
+
+public class GamePlayScreen extends AbstractScreen{
+
+	public GamePlayScreen(ProjektKCK game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- core/src/com/mygdx/screens/MenuScreen.java
+++ core/src/com/mygdx/screens/MenuScreen.java
@@ -1,10 +1,12 @@
 package com.mygdx.screens;
 
 import com.mygdx.game.ProjektKCK;
+import com.mygdx.game.characters.MainCharacter;
 import com.mygdx.ingameConsole.Console;
 public class MenuScreen extends AbstractScreen{
 	
 	private Console console;
+	public MainCharacter postka;
 	
 	public MenuScreen(ProjektKCK game) {
 		super(game);
@@ -13,6 +15,8 @@ public class MenuScreen extends AbstractScreen{
 	
 	public void create() {
 		console = new Console(600,200,50,true);
+		postka = new MainCharacter("badlogic.jpg",0,0);
+		stage.addActor(postka.image);
 		stage.addActor(console.textField);
 		stage.act();
 	}
@@ -20,6 +24,7 @@ public class MenuScreen extends AbstractScreen{
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+		GetCommendAndDoIt(console.PhraseEntered);
 		stage.act();
 		spriteBatch.begin();
 		stage.draw();
@@ -32,6 +37,25 @@ public class MenuScreen extends AbstractScreen{
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void ConsoleCommendsMenuScreen(String commend) {
+		if (commend.equals("start gry")) {
+			game.setScreen(new GamePlayScreen(game));
+		}
+		if (commend.equals("pauza")) {
+			
+		}
+		if (commend.equals("wyjscie")) {
+		}
+	
+	}
+	
+	public void GetCommendAndDoIt(boolean PhraseEntered) {
+		if (PhraseEntered == true) {
+			PhraseEntered = false;
+			ConsoleCommendsMenuScreen(console.GetText());
+		}
 	}
 
 }
