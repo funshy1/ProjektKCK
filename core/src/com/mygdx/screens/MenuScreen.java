37a59922ee794cfa5502d80616e0b7d6e0513bc0
yetju000 <yetju000steam@gmail.com>
2016-10-20 09:11:57 +0200

    background
    background

--- core/src/com/mygdx/screens/MenuScreen.java
+++ core/src/com/mygdx/screens/MenuScreen.java
@@ -1,22 +1,24 @@
 package com.mygdx.screens;
 
 import com.mygdx.game.ProjektKCK;
-import com.mygdx.game.characters.MainCharacter;
+import com.mygdx.game.actors.Actors;
 import com.mygdx.ingameConsole.Console;
-public class MenuScreen extends AbstractScreen{
-	
+
+public class MenuScreen extends AbstractScreen {
+
 	private Console console;
-	public MainCharacter postka;
-	
+	private Actors background;
+
 	public MenuScreen(ProjektKCK game) {
 		super(game);
 		create();
 	}
-	
+
 	public void create() {
-		console = new Console(600,200,50,true);
-		postka = new MainCharacter("badlogic.jpg",0,0);
-		stage.addActor(postka.image);
+		console = new Console(600, 225, 250, true);
+		background = new Actors(0, 0, "tlo.jpg");
+
+		stage.addActor(background.image);
 		stage.addActor(console.textField);
 		stage.act();
 	}
@@ -30,27 +32,19 @@ public class MenuScreen extends AbstractScreen{
 		stage.draw();
 		spriteBatch.end();
 	}
-	
-	
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
+
+	}
+
+	public void ConsoleCommendsMenuScreen(String commend) {
+		if (commend.equals("rozpocznij")) 
+			game.setScreen(new GamePlayScreen(game));
 		
 	}
-	
-	public void ConsoleCommendsMenuScreen(String commend) {
-		if (commend.equals("start gry")) {
-			game.setScreen(new GamePlayScreen(game));
-		}
-		if (commend.equals("pauza")) {
-			
-		}
-		if (commend.equals("wyjscie")) {
-		}
-	
-	}
-	
+
 	public void GetCommendAndDoIt(boolean PhraseEntered) {
 		if (PhraseEntered == true) {
 			PhraseEntered = false;
