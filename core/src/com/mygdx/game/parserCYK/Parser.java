febe82fce53a0254738875bf2a91a67a5323d5e5
bryant231 <bryant23195@gmail.com>
2016-12-21 21:39:00 +0100

    Usuniecie niepotrzebnego kodu z klasy Parser.

--- core/src/com/mygdx/game/parserCYK/Parser.java
+++ core/src/com/mygdx/game/parserCYK/Parser.java
@@ -43,9 +43,6 @@ public class Parser{
 	public static void Czytaj(int linie){
 		FileReader plik = null;
 		String linia = "";
-	
-		//System.out.println("Jestem w: ");
-		//System.out.println(Parser.class.getProtectionDomain().getCodeSource().getLocation());
 		
 		//Otworz plik
 		try {
@@ -84,13 +81,13 @@ public class Parser{
 					break;
 			}
 		}catch(IOException e){
-			System.out.println("blad odczytu");
+			System.out.println("Blad odczytu z pliku");
 			System.exit(2);
 		}
 		try{
 			plik.close();
 		}catch(IOException e){
-			System.out.println("blad zamykania pliku");
+			System.out.println("Blad zamykania pliku");
 			System.exit(3);
 		}
 	}
@@ -107,25 +104,7 @@ public class Parser{
 		}
 	
 	}
-	/*
-	public static void PoliczZasady() throws IOException{
-		//Licze ile jest zasad w pliku, grupuje po typie (Terminale, Nieterminale)
-		BufferedReader reader = new BufferedReader(new FileReader("..\\core\\assets\\Grammar\\gramatyka.txt"));
-		String linia;
-		while((linia = reader.readLine()) != null){
-			String[] wynikm = linia.split(" -> ");
-			if(wynikm[1].contains(" ")){
-				NTlines++;
-			}
-			else{
-				Tlines++;
-			}
-		}
-		
-		reader.close();
-	}
-	
-	*/
+
 	public ZasadaGramatyki[] PodajTabliceTerminali(){
 	    return(ttab);   
 	}
@@ -144,24 +123,7 @@ public class Parser{
 	
 	
 	public static String CheckType(String a)
-	{
-		
-		/*
-		String temp; temp = a;
-		if (	
-				temp.contains("A") ||
-				temp.contains("E") ||
-				temp.contains("I") ||
-				temp.contains("O") ||
-				temp.contains("U") ||
-				temp.contains("Y")
-			)
-			{temp = "A";}
-		else 
-			{temp = "B";}
-		return temp;
-		
-		*/
+	{	
 		
 		//Przeleciec tablice z terminalami i sprawdzic czy slowo sie znajudje
 		//w ktorejs z zasad
@@ -183,14 +145,6 @@ public class Parser{
 		int Pole1x;	int Pole1y;	int Pole2x;	int Pole2y;	int counter;
 		koordynatX = 0; koordynatY = 0;
 		
-		System.out.println("Tablica przed przeparsuj: ");
-		for(int i=0;i<slowa;i++){
-			for(int j=0;j<slowa;j++){
-				System.out.print(tablica[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
 		//Inicjalizacja tablicy - bez tego ryzykujemy nulle w tablicy
 		for (koordynatY = 1; koordynatY<slowa; koordynatY++)
 		{
@@ -198,17 +152,7 @@ public class Parser{
 			{
 				tablica[koordynatY][koordynatX]="N";
 			}
-		}
-		
-		System.out.println("Tablica po czyszczeniu: ");
-		for(int i=0;i<slowa;i++){
-			for(int j=0;j<slowa;j++){
-				System.out.print(tablica[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		
+		}		
 		
 		for (koordynatY = 1; koordynatY<slowa; koordynatY++)
 		{
@@ -216,21 +160,14 @@ public class Parser{
 			{
 
 				if (koordynatX<=slowa-koordynatY) {
-					System.out.println("Komorka x: " + koordynatX  + " y: "+ koordynatY);
 					
 					Pole1x = koordynatX;
 					Pole1y = 0;
 					Pole2x = koordynatX + 1;
 					Pole2y = koordynatY - 1;
-					
-					System.out.println("Pole 1x: " + Pole1x + " w iteracji po x: " + koordynatX);
-					System.out.println("Pole 1y: " + Pole1y + " w iteracji po x: " + koordynatX);
-					System.out.println("Pole 2x: " + Pole2x + " w iteracji po x: " + koordynatX);
-					System.out.println("Pole 2y: " + Pole2y + " w iteracji po x: " + koordynatX);
 					counter = 1;
+					
 					while (Pole1y < koordynatY && Pole2x < slowa) {
-						//System.out.println("Probujemy parsowac komorke " + koordynatX + koordynatY + " po raz " + counter);
-						//System.out.println("Sprawdzamy komórki " + Pole1x + Pole1y + " oraz " + Pole2x + Pole2y);
 
 						tablica[koordynatX][koordynatY] = SprawdzGramatyke(tablica, Pole1x, Pole1y, Pole2x, Pole2y);
 						counter++;
@@ -239,8 +176,6 @@ public class Parser{
 						Pole2x = Pole2x + 1;
 						Pole2y = Pole2y - 1;
 					}
-					//System.out.println("W komórce " + koordynatX + koordynatY + " znajduje sie "
-					//		+ tablica[koordynatX][koordynatY]);
 				}
 		
 			}
@@ -260,14 +195,6 @@ public class Parser{
 			}
 		}
 		
-		System.out.println("Tablica po przeparsuj: ");
-		for(int i=0;i<slowa;i++){
-			for(int j=0;j<slowa;j++){
-				System.out.print(tablica[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
 		for(int i=0;i<slowa;i++){
 			for(int j=0;j<slowa;j++){
 				if (tablica[i][j].contains("Z")){
@@ -279,37 +206,9 @@ public class Parser{
 	return "Zdania nie ma w gramatyce";
 	}
 	
-	
-	
-	
-	
-	/*
 	public static String SprawdzGramatyke(String tabliczka[][], int Pole1x, int Pole1y, int Pole2x, int Pole2y)
 	{
 		String ExitString = "";
-		//System.out.println("Sprawdzamy gramatykê");
-		if (tabliczka[Pole1x][Pole1y].contains("A") && tabliczka[Pole2x][Pole2y].contains("B")
-				|| tabliczka[Pole1x][Pole1y].contains("B") && tabliczka[Pole2x][Pole2y].contains("A"))
-		{
-			ExitString = ExitString + "S";
-		}
-		else if (tabliczka[Pole1x][Pole1y]=="S" && tabliczka[Pole2x][Pole2y].contains("B")
-				|| tabliczka[Pole1x][Pole1y].contains("B") && tabliczka[Pole2x][Pole2y]=="S" )
-		{
-			ExitString = ExitString + "C";
-		}
-		if (tabliczka[Pole1x][Pole1y].contains("A") && tabliczka[Pole2x][Pole2y]=="C"
-			|| tabliczka[Pole1x][Pole1y]=="C" && tabliczka[Pole2x][Pole2y].contains("A"))
-		{
-			ExitString = ExitString + "S";
-		}
-		return ExitString;
-	}
-	*/
-	public static String SprawdzGramatyke(String tabliczka[][], int Pole1x, int Pole1y, int Pole2x, int Pole2y)
-	{
-		String ExitString = "";
-		//System.out.println("Sprawdzamy gramatykê");
 		
 		for(int i=0;i<nttab.length;i++){
 			if (tabliczka[Pole1y][Pole1x].contains(nttab[i].PodajPS1()) && tabliczka[Pole2y][Pole2x].contains(nttab[i].PodajPS2())){
@@ -325,10 +224,11 @@ public static void main(String args[]) throws IOException{
 		//Wczytanie gramatyki
 		new Parser();
 		Parser.Czytaj(Tlines+NTlines);
-		Parser.WyswietlGramatyke();
+		//Parser.WyswietlGramatyke();
 		
 		//Wprowadzanie zdania
-		String phrase; phrase = ""; System.out.println("Input phrase");
+		String phrase; phrase = ""; 
+		//System.out.println("Input phrase");
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br=new BufferedReader(isr);
 		try
@@ -336,27 +236,18 @@ public static void main(String args[]) throws IOException{
 		catch(Exception e)
 			{System.out.println(e);}
 
-		System.out.println("The sentence is: " + phrase);
+		//System.out.println("The sentence is: " + phrase);
 		
 		//Dzielenie zdania na tokeny
 		String[] result = phrase.split("\\s+");
 		
-		//Pokazanie co mamy
-		for (int x=0; x<result.length; x++)
-			{
-				System.out.println(result[x]);
-			}
-
 		//Stworzenie tablicy o wymiarch x na x, gdzie x to ilosc slow w zdaniu
 		String Symbole[][] = new String[result.length][result.length];
 		for (int i = 0; i <result.length; i++)
 			{
 				Symbole[0][i] = CheckType(result[i]);
-				System.out.println(result[i] + " is of type " + Symbole[0][i]);
 			}
 		
-		//System.out.println("Dlugosc zdania: " + result.length);
-		//System.out.println("Badany format pokazuje koordynaty XY ");
 		String CzyJest = Przeparsuj(Symbole,result.length);
 		System.out.println(CzyJest);
 	}
